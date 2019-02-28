7adc05be5228efb60bc295c44476ff592f1283f5
krumeto97 <krumeto97@abv.bg>
2019-02-28 16:30:55 +0200

    Add files via upload

---
+++ Matrix2D/Matrix2D/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[3, 3] 
+            { 
+            { 1,2,3 }, 
+            { 4,5,6 }, 
+            { 7,8,9 }
+            };
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    System.Console.Write(array[row,col]);
+                }
+                Console.WriteLine();
+            }                   
+        }
+    }
+}
