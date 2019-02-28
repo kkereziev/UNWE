ebcedc9482e973389d3240a52524c3ef023633cd
krumeto97 <krumeto97@abv.bg>
2019-02-28 13:56:41 +0200

    Add files via upload

---
+++ Upr2/Zad1/Zad1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Zad1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <=10; i++)
+            {
+                Console.Write($"{number}*{i}=");
+                Console.WriteLine(number*i);
+            }
+        }
+    }
+}
---
+++ Upr2/Zad4/Zad4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad4
+{
+    class Zad4
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int[] array1 = new int[a];
+            int[] array2 = new int[b];
+            bool isTrue=true;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array2.Length; i++)
+            {
+                array2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            if (array1.Length==array2.Length)
+            {
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    if (array1[i]!=array2[i])
+                    {
+                        Console.WriteLine("Масивите не са еднакви, но имат еднаква дължина.");
+                        break;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Масивите са еднакви.");
+                    }                    
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Масивите не са еднакви.");
+            }
+        }
+    }
+}
