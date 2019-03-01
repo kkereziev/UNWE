b4e2de3dae7048f71adab1edbec72a2940761f55
krumeto97 <krumeto97@abv.bg>
2019-03-01 17:18:23 +0200

    Add files via upload

---
+++ Domashno1/Domashno1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashno1
+{
+    class Domashno1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the lenght of the array: ");
+            int a = int.Parse(Console.ReadLine());
+            int[] array = new int[a];
+            int count = 1;
+            int tempCount;
+            int common = array[0];
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("=========");                                 
+            for (int i = 0; i < array.Length; i++)
+            {
+                temp = array[i];
+                tempCount = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (temp == array[j])
+                    {
+                        tempCount++;
+                    }
+                }
+                if (tempCount > count)
+                {
+                    common = temp;
+                    count = tempCount;
+
+                }
+
+            }
+            Console.Write("Most common element: ");
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{common},");
+            }
+            Console.WriteLine();
+        }      
+    }
+}
---
+++ Domashno2/Domashno2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashno2
+{
+    class Domashno2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int lenght = Int32.Parse(Console.ReadLine());
+            int[] array = new int[lenght];
+            int same = 1, bestSame = 1, bestStart = 0, lastElement = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (array[i]+1==array[i+1])
+                {
+                    same++;
+                    if (same>bestSame)
+                    {
+                        bestSame = same;
+                        lastElement = i + 1;
+                        bestStart = lastElement-bestSame + 1;
+                    }
+                }
+                else
+                {
+                    same = 1;
+                }
+            }
+            for (int i = bestStart; i < bestSame+bestStart; i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
